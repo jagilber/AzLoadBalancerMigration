fc1da25847f402a9797580689b740f0c1c50df8c
Jason Gilbertson <jagilber@users.noreply.github.com>
2023-02-05 17:35:49 -0500

    only log verbose if requsted

--- AzureBasicLoadBalancerUpgrade/modules/Job/Job.psm1
+++ AzureBasicLoadBalancerUpgrade/modules/Job/Job.psm1
@@ -42,7 +42,9 @@ function WaitJob {
             log -Message "[WaitJob] Receiving Job: $($jobInfo)"
         }
         else {
-            log -Message "[WaitJob] Receiving Job No Update: $($job | ConvertTo-Json -Depth 1 -WarningAction SilentlyContinue)" -Severity "Debug"
+            if($VerbosePreference -ieq 'Continue') {
+                log -Message "[WaitJob] Receiving Job No Update: $($job | ConvertTo-Json -Depth 1 -WarningAction SilentlyContinue)" -Severity "Verbose"
+            }
         }
 
         $executionTime = ((get-date) - $job.PSBeginTime).Minutes
